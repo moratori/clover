567dc28d0561c8b12cb01d7508ba62e12932bf84
moratori <hoko1993@yahoo.co.jp>
2018-09-18 23:38:43 +0900

    網羅的に導出可能な節を生成する方法を実装

--- src/resolution.lisp
+++ src/resolution.lisp
@@ -6,11 +6,12 @@
         :clover.util
         :clover.unify
         :clover.simplify
+        :clover.rename
         :clover.substitute
         )
   (:export
     :%collect-resolutable-literal
-    :resolution
+    :%resolution
     )
   )
 (in-package :clover.resolution)
@@ -33,7 +34,7 @@
           :collect (resolution-candidate literal1 unifier-set-list))))
 
 
-(defmethod resolution ((clause1 clause) (clause2 clause))
+(defmethod %resolution ((clause1 clause) (clause2 clause))
   "clause1 と clause2を導出することで得ることの可能な節clauseの全てをリストで得る"
   (let ((res-clause (clause (append (clause.literals clause1)
                                     (clause.literals clause2)))))
@@ -47,20 +48,46 @@
                       (apply-unifier-set focus-literal us))
                     (unifiered-clause
                       (apply-unifier-set res-clause us)))
-                (simplify
-                  (clause
-                    (remove-if
-                      (lambda (lit)
-                        (or 
-                          (literal= unifiered-focus-literal lit)
-                          (complement-literal-p unifiered-focus-literal lit)))
-                      (clause.literals unifiered-clause))
-                    (when *save-resolution-history* clause1)
-                    (when *save-resolution-history* clause2)
-                    (when *save-resolution-history* focus-literal)
-                    (when *save-resolution-history* us)))))
+                (clause
+                  (remove-if
+                    (lambda (lit)
+                      (or 
+                        (literal= unifiered-focus-literal lit)
+                        (complement-literal-p unifiered-focus-literal lit)))
+                    (clause.literals unifiered-clause))
+                  (when *save-resolution-history* clause1)
+                  (when *save-resolution-history* clause2)
+                  (when *save-resolution-history* focus-literal)
+                  (when *save-resolution-history* us))))
             unifier-set-list)))
       (%collect-resolutable-literal
         clause1
         clause2))))
+
+
+(defmethod %prepare-resolution ((clause-set clause-set))
+  (rename
+    (simplify clause-set)))
+
+
+
+(defmethod opener_exhaustive-worst-resolution ((clause-set clause-set))
+  (let* ((prepared 
+           (%prepare-resolution clause-set))
+         (clauses
+           (clause-set.clauses prepared))
+         (next-each-nodes
+           (loop :for clause1 :in clauses
+                 :for i :from 0 
+                 :append
+                 (loop :for clause2 :in clauses
+                       :for j :from 0
+                       :if (> j i)
+                       :append (%resolution clause1 clause2)))))
+    (mapcar 
+      (lambda (clause)
+        (clause-set
+          (cons clause clauses)))
+      next-each-nodes)))
+
 
