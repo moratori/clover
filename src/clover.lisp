968114bd7e87d7cd4a53bf56d0f51b4ed2ea4554
moratori <hoko1993@yahoo.co.jp>
2018-09-22 01:41:22 +0900

    導出のトップレベル処理を定義

--- src/clover.lisp
+++ src/clover.lisp
@@ -1,21 +1,29 @@
 (defpackage clover.clover
   (:use :cl
+        :clover.property
         :clover.search.iddfs
         :clover.types
-        :clover.unify
+        :clover.resolution
         )
-  (:export 
-        :open-nodes
-        :finish))
+  (:export
+    :start_resolution
+    ))
 (in-package :clover.clover)
 
 
-
 (defmethod open-nodes ((clause-set clause-set))
-  )
+  (case *resolution-algorithm*
+    (:worst (opener_exhaustive-worst-resolution clause-set))
+    (otherwise (error 
+                 (make-condition 'unimplemented-resolution-algorithm
+                                 :message (format nil "unimplemented resolution algorithm"))))))
 
 
 (defmethod finish ((clause-set clause-set))
   (some (lambda (x)
           (null (clause.literals x))) 
         (clause-set.clauses clause-set)))
+
+
+(defmethod start_resolution ((clause-set clause-set))
+  (iddfs clause-set *resolution-search-depth*))
