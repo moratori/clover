c3e53b1b2c7013f377dfb9c862a82e914756be46
moratori <hoko1993@yahoo.co.jp>
2018-09-23 13:45:42 +0900

    %resolutionの中で処理をまとめるよう修正 親節から、どのリテラルを削除してよいかわからなくなるため

--- src/resolution.lisp
+++ src/resolution.lisp
@@ -19,7 +19,7 @@
 
 
 
-(defmethod %collect-resolutable-literal ((clause1 clause) (clause2 clause))
+(defmethod %resolution ((clause1 clause) (clause2 clause))
   (let ((literals1 (clause.literals clause1))
         (literals2 (clause.literals clause2)))
     (loop :for literal1 :in literals1
@@ -31,39 +31,26 @@
                                   (ununifiable-literal-error (e) nil))
                      :if us 
                      :collect us)
-          :if unifier-set-list
-          :collect (resolution-candidate literal1 unifier-set-list))))
-
-
-(defmethod %resolution ((clause1 clause) (clause2 clause))
-  "clause1 と clause2を導出することで得ることの可能な節clauseの全てをリストで得る"
-  (let ((res-clause (clause (append (clause.literals clause1)
-                                    (clause.literals clause2)))))
-    (mapcan
-      (lambda (rc)
-        (let ((focus-literal    (res-cand.focus-literal rc))
-              (unifier-set-list (res-cand.cand-unifiers rc)))
-          (mapcar 
-            (lambda (us)
-              (let ((unifiered-focus-literal 
-                      (apply-unifier-set focus-literal us))
-                    (unifiered-clause
-                      (apply-unifier-set res-clause us)))
-                (clause
-                  (remove-if
-                    (lambda (lit)
-                      (or 
-                        (literal= unifiered-focus-literal lit)
-                        (complement-literal-p unifiered-focus-literal lit)))
-                    (clause.literals unifiered-clause))
-                  (when *save-resolution-history* clause1)
-                  (when *save-resolution-history* clause2)
-                  (when *save-resolution-history* focus-literal)
-                  (when *save-resolution-history* us))))
-            unifier-set-list)))
-      (%collect-resolutable-literal
-        clause1
-        clause2))))
+          :if unifier-set-list 
+          :append (loop :for unifier-set :in unifier-set-list
+                        :for res-clause-left  = (apply-unifier-set clause1 unifier-set)
+                        :for res-clause-left-literals = (clause.literals res-clause-left)
+                        :for res-clause-right = (apply-unifier-set clause2 unifier-set)
+                        :for res-clause-right-literals = (clause.literals res-clause-right)
+                        :for target-literal   = (apply-unifier-set literal1 unifier-set)
+                        :collect
+                        (clause 
+                          (append 
+                            (remove-if 
+                              (lambda (l) (literal= target-literal l))
+                              res-clause-left-literals)
+                            (remove-if 
+                              (lambda (l) (complement-literal-p target-literal l))
+                              res-clause-right-literals))
+                          (when *save-resolution-history* clause1)
+                          (when *save-resolution-history* clause2)
+                          (when *save-resolution-history* literal1)
+                          (when *save-resolution-history* unifier-set))))))
 
 
 (defmethod %prepare-resolution ((clause-set clause-set))
