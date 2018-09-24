85b9decc0ffe154ccb0314471b9b73950e5a6c37
moratori <hoko1993@yahoo.co.jp>
2018-09-24 22:42:20 +0900

    テストケース追加

--- tests/simplify.lisp
+++ tests/simplify.lisp
@@ -8,6 +8,55 @@
 (in-package :clover.tests.simplify)
 
 
+
+(test clover.tests.simplify.%remove-independent-clause
+
+      (is 
+        (let*((clause1
+                (clause (list (literal nil 'P (list (fterm 'A nil)))
+                              (literal nil 'P (list (fterm 'g (list (vterm 'x))))))))
+              (clause2
+                (clause (list (literal nil 'P (list (vterm 'x)))
+                              (literal nil 'P (list (fterm 'h nil))))))
+              (clauses
+                (list clause1 clause2))
+              (expected
+                (clause-set nil)))
+          (clause-set= (clause-set (clover.simplify::%remove-independent-clause clauses))
+                       expected)))
+      
+      (is 
+        (let*((clause1
+                (clause (list (literal t 'P (list (fterm 'A nil)))
+                              (literal nil 'P (list (fterm 'g (list (vterm 'x))))))))
+              (clause2
+                (clause (list (literal nil 'Q (list (vterm 'x)))
+                              (literal nil 'P (list (fterm 'h nil))))))
+              (clauses
+                (list clause1 clause2))
+              (expected
+                (clause-set (list 
+                              (clause (list (literal t 'P (list (fterm 'A nil)))
+                                            (literal nil 'P (list (fterm 'g (list (vterm 'x)))))))))))
+          (clause-set= (clause-set (clover.simplify::%remove-independent-clause clauses))
+                       expected)))
+      
+      (is 
+        (let*((clause1
+                (clause (list (literal t 'P (list (fterm 'A nil)))
+                              (literal t 'P (list (fterm 'g (list (vterm 'x))))))))
+              (clause2
+                (clause (list (literal t 'P (list (vterm 'x)))
+                              (literal t 'P (list (fterm 'h nil))))))
+              (clauses
+                (list clause1 clause2))
+              (expected
+                (clause-set nil)))
+          (clause-set= (clause-set (clover.simplify::%remove-independent-clause clauses))
+                       expected)))
+      )
+
+
 (test clover.tests.simplify.simplify
 
       (is 
