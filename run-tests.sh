6f15c65eccfd87d623791f47e0dce50064465086
moratori <hoko1993@yahoo.co.jp>
2018-10-07 19:33:25 +0900

    リファクタリング

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+#### roswell が見当たらない場合は終了
 roswell="`/usr/bin/which ros`"
 if [ ${?} -ne 0 ]; then
         echo "Roswell required: https://github.com/roswell/roswell"
@@ -8,27 +8,54 @@ if [ ${?} -ne 0 ]; then
 fi
 
 
-lisp_implementation="`ros -e '(princ (string-downcase (lisp-implementation-type)))'`"
 
-if [ "${lisp_implementation}" = "sbcl" ]; then
-        $roswell -s sb-cover \
-                 -s clover-test  \
-                 -e '(1am:run)' \
-                 -e '(sb-cover:report (merge-pathnames #P"coverage/" (asdf:system-source-directory :clover)))'
-else
-        $roswell -s clover-test \
-                 -e '(1am:run)'
-fi
+#### 各種変数定義
+lisp_implementation="`$roswell -e '(princ (string-downcase (lisp-implementation-type)))'`"
+lisp_implementation_version="`$roswell -e '(princ (lisp-implementation-version))'`"
 
+##   プロジェクトルートを取得する(sbclの場合はsb-coverを読ませたいので呼び分ける)
+case "${lisp_implementation}" in 
+        "sbcl" ) project_root="`$roswell -s sb-cover -e '(princ (asdf:system-source-directory :clover))'`" ;;
+        *      ) project_root="`$roswell -e '(princ (asdf:system-source-directory :clover))'`" ;;
+esac
 
-dot="`/usr/bin/which dot`"
-if [ ${?} -ne 0 ]; then
-        echo "Graphviz required for rendering refutation tree"
+if [ ! -d "${project_root}" ]; then
+        echo "can't get project root properly : ${project_root}"
         exit 1
 fi
 
-dot_src_path="test-output-files/"
+project_root="${project_root%/}"
+dot_src_path="${project_root}/test-output-files/graphviz/"
+coverage_path="${project_root}/coverage/"
 
-if [ -d "${dot_src_path}" ]; then
-        find $dot_src_path -type f -name "*.dot" -exec dot -Tpng -o {}.png {} \;
+
+
+#### テスト実行結果等を出力するためのディレクトリを作成
+/bin/mkdir -p "${dot_src_path}"
+/bin/mkdir -p "${coverage_path}"
+
+
+
+#### 処理系ごとにテスト実行(sbclの場合はcoverageを出力したいため呼び分ける)
+case "${lisp_implementation}" in 
+        "sbcl" ) $roswell -s sb-cover \
+                   -s clover-test  \
+                   -e '(1am:run)' \
+                   -e '(sb-cover:report (merge-pathnames #P"coverage/" (asdf:system-source-directory :clover)))';;
+        *      ) $roswell -s clover-test \
+                   -e '(1am:run)'
+esac
+
+
+
+#### Graphvizが存在する場合は、描画まで実行する
+dot="`/usr/bin/which dot`"
+if [ ${?} -eq 0 ]; then
+        if [ -d "${dot_src_path}" ]; then
+                /usr/bin/find $dot_src_path -type f -name "*.dot" -exec $dot -Tpng -o {}.png {} \;
+        fi
+else
+        echo "Graphviz required for rendering refutation tree"
+        echo "test skipped"
+        exit 1
 fi
