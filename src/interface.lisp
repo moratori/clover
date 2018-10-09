99cc75ceb915c2acb8e9b1cbb50485ce65d35b3b
moratori <hoko1993@yahoo.co.jp>
2018-10-09 23:01:29 +0900

    コマンドラインインターフェースの実装用モジュール追加

---
+++ src/interface.lisp
@@ -0,0 +1,12 @@
+(defpackage clover.interface
+  (:use :cl
+        :clover.property
+        :clover.conditions
+        :clover.types
+        :clover.parser
+        :clover.util
+        )
+  (:export ))
+(in-package :clover.interface)
+
+
