9b9a26c4b7babfc3d523294531949c6381ad350b
moratori <hoko1993@yahoo.co.jp>
2018-10-14 13:42:31 +0900

    パーサに対するテストケースを追加

---
+++ tests/parser.lisp
@@ -0,0 +1,115 @@
+(defpackage clover.tests.parser
+  (:use :cl
+        :clover.property
+        :clover.types
+        :clover.parser
+        :clover.util
+        :1am))
+(in-package :clover.tests.parser)
+
+
+(test clover.tests.parser.parse-premise-logical-expression
+      
+      (is (clause=
+               (parse-premise-logical-expression "pred(x)")
+               (clause 
+                 (list 
+                   (literal nil 
+                            'CLOVER.PARSER::PRED
+                            (list (vterm 'CLOVER.PARSER::X))))))) 
+      
+      (is (clause=
+               (parse-premise-logical-expression "!pred(x)")
+               (clause 
+                 (list 
+                   (literal t
+                            'CLOVER.PARSER::PRED
+                            (list (vterm 'CLOVER.PARSER::X)))))))
+      
+      (is (clause=
+               (parse-premise-logical-expression "!pred(x,y)")
+               (clause 
+                 (list 
+                   (literal t
+                            'CLOVER.PARSER::PRED
+                            (list (vterm 'CLOVER.PARSER::X)
+                                  (vterm 'CLOVER.PARSER::Y)))))))
+      
+      (is (clause=
+               (parse-premise-logical-expression "!pred(f(x),y)")
+               (clause 
+                 (list 
+                   (literal t
+                            'CLOVER.PARSER::PRED
+                            (list (fterm 'CLOVER.PARSER::F (list (vterm 'CLOVER.PARSER::X)))
+                                  (vterm 'CLOVER.PARSER::Y)))))))
+      
+      (is (clause=
+               (parse-premise-logical-expression "!pred(y,f(x))")
+               (clause 
+                 (list 
+                   (literal t
+                            'CLOVER.PARSER::PRED
+                            (list (vterm 'CLOVER.PARSER::Y)
+                                  (fterm 'CLOVER.PARSER::F (list (vterm 'CLOVER.PARSER::X)))))))))
+      
+      (is (clause=
+               (parse-premise-logical-expression "!pred(g(y),f(x))")
+               (clause 
+                 (list 
+                   (literal t
+                            'CLOVER.PARSER::PRED
+                            (list (fterm 'CLOVER.PARSER::G (list (vterm 'CLOVER.PARSER::Y)))
+                                  (fterm 'CLOVER.PARSER::F (list (vterm 'CLOVER.PARSER::X)))))))))
+      
+      (is (clause=
+               (parse-premise-logical-expression "!pred(A,f(x))")
+               (clause 
+                 (list 
+                   (literal t
+                            'CLOVER.PARSER::PRED
+                            (list (fterm 'CLOVER.PARSER::A nil)
+                                  (fterm 'CLOVER.PARSER::F (list (vterm 'CLOVER.PARSER::X)))))))))
+      
+      (is (clause=
+               (parse-premise-logical-expression "!pred(f(x),A)")
+               (clause 
+                 (list 
+                   (literal t
+                            'CLOVER.PARSER::PRED
+                            (list (fterm 'CLOVER.PARSER::F (list (vterm 'CLOVER.PARSER::X)))
+                                  (fterm 'CLOVER.PARSER::A nil)))))))
+      
+      (is (clause=
+               (parse-premise-logical-expression "!pred(g(f(x)),A)")
+               (clause 
+                 (list 
+                   (literal t
+                            'CLOVER.PARSER::PRED
+                            (list (fterm 'CLOVER.PARSER::G (list (fterm 'CLOVER.PARSER::F (list (vterm 'CLOVER.PARSER::X)))))
+                                  (fterm 'CLOVER.PARSER::A nil)))))))
+
+      (is (clause=
+               (parse-premise-logical-expression "!pred(g(f(x)),A) | q(A, B) | !r(x,y,fun(ABC))")
+               (clause 
+                 (list 
+                   (literal t
+                            'CLOVER.PARSER::PRED
+                            (list (fterm 'CLOVER.PARSER::G (list (fterm 'CLOVER.PARSER::F (list (vterm 'CLOVER.PARSER::X)))))
+                                  (fterm 'CLOVER.PARSER::A nil)))
+                   (literal nil
+                            'CLOVER.PARSER::Q
+                            (list (fterm 'CLOVER.PARSER::A nil)
+                                  (fterm 'CLOVER.PARSER::B nil)))
+                   (literal t
+                            'CLOVER.PARSER::R
+                            (list (vterm 'CLOVER.PARSER::X)
+                                  (vterm 'CLOVER.PARSER::Y)
+                                  (fterm 'CLOVER.PARSER::FUN (list (fterm 'CLOVER.PARSER::ABC nil)))))))))
+      )
+
+
+
+
+
+
