13fb912f29670d82d809fe9f2104474af09eb01b
moratori <hoko1993@yahoo.co.jp>
2020-11-22 12:36:46 +0900

    探索開始ノードから見つけたノードまでの最短距離を正常に出せないバグ、並びに そもそもマルチスレッド化しても効率的になるタイプの探索ではないので、実装を削除

--- src/search/iddfs.lisp
+++ src/search/iddfs.lisp
@@ -1,66 +1,13 @@
 (defpackage clover.search.iddfs
-  (:use :cl
-        :bordeaux-threads
-        :cl-cpus)
+  (:use :cl)
   (:export 
     :abstract-node
     :open-nodes
     :finish
     :iddfs
-    :*minimal-number-of-worker-processors*
     )
   )
 (in-package :clover.search.iddfs)
-
-
-(defparameter *minimal-number-of-worker-processors* 3)
-
-
-(defun make-thread-pools (maximum-limit number-of-processors)
-  (declare (fixnum number-of-processors))
-  (let* ((seed 
-           (loop for i from 0 upto maximum-limit collect i))
-         (len (length seed))
-         (number-of-regular-pools
-           (floor (/ len number-of-processors)))
-         (rest-of-pools
-           (mod len number-of-processors)))
-    (append
-      (loop for i from 1 upto number-of-regular-pools
-            for start = (* (1- i) number-of-processors)
-            for end   = (+ start number-of-processors)
-            collect (subseq seed start end))
-      (unless (zerop rest-of-pools)
-        (list (subseq seed (- len rest-of-pools) len))))))
-
-(defun run-pool (initial-node pool)
-  (loop for deepth in pool
-        collect 
-        (cons 
-          deepth
-          (make-thread
-            (lambda () (%iddfs-main initial-node deepth))))))
-
-(defun found-pool (pool)
-  (let (cnt result) 
-    (loop 
-      named exit
-      for (deepth . thread) in pool
-      do 
-      (unless (thread-alive-p thread)
-        (multiple-value-bind (flag found) (join-thread thread)
-          (when flag
-            (setf cnt deepth
-                  result found)
-            (return-from exit nil)))))
-    (values cnt result)))
-
-(defun alive-pool-p (pool)
-  (some 
-    (lambda (thread)
-      (destructuring-bind (_ . thread) thread
-          (thread-alive-p thread))) 
-    pool))
 
 
 (defstruct abstract-node)
@@ -75,42 +22,6 @@
 
 (defmethod iddfs ((initial-node abstract-node) maximum-limit)
   (declare (fixnum maximum-limit))
-  (let* ((number-of-processors 
-           (get-number-of-processors))
-         (valid-number-of-processors
-           (1- number-of-processors)))
-    (cond
-      ((>= valid-number-of-processors *minimal-number-of-worker-processors*)
-       (%iddfs-multi initial-node maximum-limit valid-number-of-processors))
-      (t 
-       (%iddfs-single initial-node maximum-limit)))))
-
-
-(defun %iddfs-multi (initial-node maximum-limit number-of-processors)
-  (declare (fixnum maximum-limit number-of-processors))
-  (let* ((thread-pools
-           (make-thread-pools maximum-limit number-of-processors))
-         (cnt nil)
-         (result nil))
-    (loop 
-      named exit
-      for pool in thread-pools
-      for threads-pool = (run-pool initial-node pool)
-      do
-      (progn
-        (loop 
-          while (alive-pool-p threads-pool)
-          do (progn))
-        (multiple-value-bind (local-cnt local-found) (found-pool threads-pool)
-          (when local-cnt
-            (setf cnt local-cnt
-                  result local-found)
-            (return-from exit nil)))))
-    (values cnt result)))
-
-
-(defun %iddfs-single (initial-node maximum-limit)
-  (declare (fixnum maximum-limit))
   (let (cnt result)
     (loop
       named exit
@@ -123,7 +34,6 @@
                 result value)
           (return-from exit nil))))
     (values cnt result)))
-
 
 (defun %iddfs-main (initial-node deepth)
   (declare (fixnum deepth))
