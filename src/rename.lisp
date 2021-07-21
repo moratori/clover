9738a42e20fb22c9cb1619916b243d00b459d4c4
moratori <hoko1993@yahoo.co.jp>
2021-07-21 12:52:07 +0900

    equationとrewrite-ruleに対してもreadable-printingを実装

--- src/rename.lisp
+++ src/rename.lisp
@@ -41,6 +41,34 @@
           (clause.literals clause))
         :test #'term=))))
 
+(defmethod make-rename-binds ((rewrite-rule rewrite-rule))
+  (unifier-set
+    (mapcar 
+      (lambda (term)
+        (unifier 
+          term (vterm (gensym *vterm-gensym-prefix*))))
+      (remove-duplicates
+        (mapcan
+          #'collect-variables
+          (list 
+            (rewrite-rule.src rewrite-rule)
+            (rewrite-rule.dst rewrite-rule)))
+        :test #'term=))))
+
+(defmethod make-rename-binds ((equation equation))
+  (unifier-set
+    (mapcar 
+      (lambda (term)
+        (unifier 
+          term (vterm (gensym *vterm-gensym-prefix*))))
+      (remove-duplicates
+        (mapcan
+          #'collect-variables
+          (list 
+            (equation.left equation)
+            (equation.right equation)))
+        :test #'term=))))
+
 (defmethod rename ((rewrite-rule rewrite-rule))
   (let* ((src (rewrite-rule.src rewrite-rule))
          (dst (rewrite-rule.dst rewrite-rule))
@@ -92,4 +120,54 @@
          (apply-unifier-set
            clause
            readable-bind))))))
+
+(defmethod rename-for-human-readable-printing ((equation equation))
+  (let* ((unifier-set (make-rename-binds equation))
+         (unif (unifier-set.unifiers unifier-set)))
+    (cond 
+      ((> (length unif) (length *vterm-for-human-readable*))
+       ;; 変数の数が多すぎるので変換せずに返却
+       equation)
+      (t 
+       (let ((readable-bind
+               (unifier-set
+                 (loop :for u :in unif
+                       :for sym :in *vterm-for-human-readable*
+                       :collect
+                       (unifier
+                         (unifier.src u)
+                         (vterm (%intern-symbol-to-specified-package sym)))))))
+         (equation
+           (equation.negation equation)
+           (apply-unifier-set 
+             (equation.left equation) readable-bind)
+           (apply-unifier-set
+             (equation.right equation) readable-bind)))))))
+
+(defmethod rename-for-human-readable-printing ((equation-set equation-set))
+  (equation-set
+    (mapcar #'rename-for-human-readable-printing 
+            (equation-set.equations equation-set))))
+
+(defmethod rename-for-human-readable-printing ((rewrite-rule rewrite-rule))
+  (let* ((unifier-set (make-rename-binds rewrite-rule))
+         (unif (unifier-set.unifiers unifier-set)))
+    (cond 
+      ((> (length unif) (length *vterm-for-human-readable*))
+       ;; 変数の数が多すぎるので変換せずに返却
+       rewrite-rule)
+      (t 
+       (let ((readable-bind
+               (unifier-set
+                 (loop :for u :in unif
+                       :for sym :in *vterm-for-human-readable*
+                       :collect
+                       (unifier
+                         (unifier.src u)
+                         (vterm (%intern-symbol-to-specified-package sym)))))))
+         (rewrite-rule
+           (apply-unifier-set 
+             (rewrite-rule.src rewrite-rule) readable-bind)
+           (apply-unifier-set
+             (rewrite-rule.dst rewrite-rule) readable-bind)))))))
 
