7d569e3348d0e883ba63bf73a3bf6874b4ed97af
moratori <hoko1993@yahoo.co.jp>
2021-07-24 00:21:29 +0900

    テストケース追加

--- tests/src/completion.lisp
+++ tests/src/completion.lisp
@@ -11,7 +11,6 @@
 (in-package :clover.tests.completion)
 
 
-
 (test clover.tests.completion.kb-completion.test1
       (setf *term-order-algorithm* :original)
 
@@ -20,20 +19,49 @@
                 (list
                   (equation 
                     nil
-                    (vterm 'x)
-                    (fterm 'plus (list (constant 'ZERO) (vterm 'x))))
-                  (equation
-                    nil
-                    (constant 'ZERO)
-                    (fterm 'plus (list (fterm 'inv (list (vterm 'x)))
-                                       (vterm 'x))))
-                  (equation 
-                    nil
-                    (fterm 'plus (list (vterm 'x)
-                                       (fterm 'plus (list (vterm 'y) (vterm 'z)))))
-                    (fterm 'plus (list (fterm 'plus (list (vterm 'x) (vterm 'y))) 
-                                       (vterm 'z)))))))
+                    (fterm 'h (list (fterm 'h (list (vterm 'x)))))
+                    (fterm 'g (list (vterm 'x)))))))
             (result
-              (kb-completion target 100)))
-        (print (rename-for-human-readable-printing result))
-        (is (and result (not (null (rewrite-rule-set.rewrite-rules result)))))))
+              (rename-for-human-readable-printing
+                (kb-completion target 10)))
+            (expected
+              (rewrite-rule-set
+                (list 
+                  (rewrite-rule
+                    (fterm 'h (list (fterm 'h (list (vterm 'CLOVER.PARSER::X)))))
+                    (fterm 'g (list (vterm 'CLOVER.PARSER::X))))
+                  (rewrite-rule
+                    (fterm 'h (list (fterm 'g (list (vterm 'CLOVER.PARSER::X)))))
+                    (fterm 'g (list (fterm 'h (list (vterm 'CLOVER.PARSER::X)))))
+                    )
+                  ))))
+
+        (is (rewrite-rule-set= result expected))))
+
+
+;(test clover.tests.completion.kb-completion.test2
+;      (setf *term-order-algorithm* :original)
+;      (setf CLOVER.COMPLETION::*debug-print* 0.1)
+;
+;      (let* ((target 
+;              (equation-set
+;                (list
+;                  (equation 
+;                    nil
+;                    (vterm 'x)
+;                    (fterm 'plus (list (constant 'ZERO) (vterm 'x))))
+;                  (equation
+;                    nil
+;                    (constant 'ZERO)
+;                    (fterm 'plus (list (fterm 'inv (list (vterm 'x)))
+;                                       (vterm 'x))))
+;                  (equation 
+;                    nil
+;                    (fterm 'plus (list (vterm 'x)
+;                                       (fterm 'plus (list (vterm 'y) (vterm 'z)))))
+;                    (fterm 'plus (list (fterm 'plus (list (vterm 'x) (vterm 'y))) 
+;                                       (vterm 'z)))))))
+;            (result
+;              (kb-completion target 100)))
+;        (print (rename-for-human-readable-printing result))
+;        (is (and result (not (null (rewrite-rule-set.rewrite-rules result)))))))
