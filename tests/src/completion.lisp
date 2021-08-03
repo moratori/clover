c63af711460b5f3918802edfabb7e20f8163a954
moratori <hoko1993@yahoo.co.jp>
2021-08-03 13:30:46 +0900

    テストケース追加

--- tests/src/completion.lisp
+++ tests/src/completion.lisp
@@ -75,6 +75,90 @@
                            (fterm 'f (list (vterm 'u) (vterm 'v)))
                            (vterm 'u))))))))))
 
+(test clover.tests.completion.collapse-rule.test3
+      (let ((initial-e
+              (equation-set nil))
+            (initial-r
+              (rewrite-rule-set
+                (list
+                   (rewrite-rule
+                     (fterm 'plus (list (constant 'ZERO) (vterm 'x)))
+                     (vterm 'x))
+                   (rewrite-rule
+                     (fterm 'plus (list (fterm 'inv (list (vterm 'x))) (vterm 'x)))
+                     (constant 'ZERO))
+                   (rewrite-rule
+                     (fterm 'plus (list (fterm 'plus (list (vterm 'x) (vterm 'y))) (vterm 'z)))
+                     (fterm 'plus (list (vterm 'x) (fterm 'plus (list (vterm 'y) (vterm 'z))))))
+                   (rewrite-rule
+                     (fterm 'plus (list (fterm 'inv (list (vterm 'x))) (fterm 'plus (list (vterm 'x) (vterm 'y)))))
+                     (vterm 'y))
+                   (rewrite-rule
+                     (fterm 'plus (list (vterm 'x) (constant 'ZERO)))
+                     (vterm 'x))
+                   (rewrite-rule
+                     (fterm 'inv (list (fterm 'inv (list (vterm 'x)))))
+                     (vterm 'x))
+                   (rewrite-rule
+                     (fterm 'plus (list (vterm 'x) (fterm 'inv (list (vterm 'x)))))
+                     (constant 'ZERO))
+                   (rewrite-rule
+                     (fterm 'inv (list (constant 'ZERO)))
+                     (constant 'ZERO))
+                   (rewrite-rule
+                     (fterm 'plus (list (vterm 'x) (fterm 'plus (list (fterm 'inv (list (vterm 'x))) (vterm 'y)))))
+                     (vterm 'y))
+                   (rewrite-rule
+                     (fterm 'inv (list (fterm 'plus (list (vterm 'x) (vterm 'y)))))
+                     (fterm 'plus (list (fterm 'inv (list (vterm 'y)))
+                                        (fterm 'inv (list (vterm 'x)))))))
+                )))
+        (multiple-value-bind (result-e result-r)
+            (clover.completion::infer :collapse initial-e initial-r)
+          (is (and (equation-set= 
+                     result-e
+                     (equation-set nil))
+                   (rewrite-rule-set= 
+                     result-r
+                     initial-r))))))
+
+(test clover.tests.completion.collapse-rule.test4
+      (let ((initial-e
+              (equation-set nil))
+            (initial-r
+              (rewrite-rule-set
+                (list
+                  (rewrite-rule
+                    (fterm 'append (list (constant 'NIL) (vterm 'x)))
+                    (vterm 'x))
+                  (rewrite-rule
+                    (fterm 'append (list (fterm 'cons (list (vterm 'x) 
+                                                            (vterm 'y)))
+                                         (vterm 'z)))
+                    (fterm 'cons (list (vterm 'x) (fterm 'append (list (vterm 'y) (vterm 'z))))))
+                  (rewrite-rule
+                    (fterm 'reverse (list (constant 'NIL)))
+                    (constant 'NIL))
+                  (rewrite-rule
+                    (fterm 'reverse (list (fterm 'reverse (list (vterm 'x)))))
+                    (vterm 'x)
+                    )
+                  (rewrite-rule
+                    (fterm 'reverse (list (fterm 'cons (list (vterm 'x) (vterm 'y)))))
+                    (fterm 'append (list (fterm 'reverse (list (vterm 'y)))
+                                         (fterm 'cons (list (vterm 'x) (constant 'NIL))))))
+                  (rewrite-rule
+                    (fterm 'reverse (list (fterm 'append (list (vterm 'x) (fterm 'cons (list (vterm 'y) (constant 'NIL)))))))
+                    (fterm 'cons (list (vterm 'y) (fterm 'reverse (list (vterm 'x))))))))))
+        (multiple-value-bind (result-e result-r)
+            (clover.completion::infer :collapse initial-e initial-r)
+          (is (and (equation-set= 
+                     result-e
+                     (equation-set nil))
+                   (rewrite-rule-set= 
+                     result-r
+                     initial-r))))))
+
 (test clover.tests.completion.deduce-rule.test1
       (let ((initial-e
               (equation-set nil))
