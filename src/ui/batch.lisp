3db5eb5ed792570d0056e17818f6cfc148b835a4
moratori <hoko1993@yahoo.co.jp>
2021-12-19 00:52:33 +0900

    rewriteコマンドを実装

--- src/ui/batch.lisp
+++ src/ui/batch.lisp
@@ -10,6 +10,10 @@
         :clover.ui.util
         :ppcre
         )
+  (:import-from :clover.parser
+                :parse-premise-logical-expression)
+  (:import-from :clover.rewrite
+                :rewrite-final)
   (:import-from :clover.multicompletion
                 :multi-kb-completion)
   (:import-from :clover.rename
@@ -24,7 +28,10 @@
 (defun help ()
   (%stdout
     "Command help
-     complete <file name>       execute completion process~%")
+     complete <file name>                        execute completion process
+     rewrite  <file name> <term-1> ... <term-n>  execute completion and get irreducible term
+                                                 lowercase alphabet appeared in term is treated as a variable
+                                                 uppercase alphabet is treated as a constant~%")
   )
 
 
@@ -38,6 +45,63 @@
     (t 
      (%stdout "invalid command argument: ~A~%" args))))
 
+(defmethod %perform-command :around ((command (eql :REWRITE)) args)
+  (cond 
+    ((and 
+          (<= 2 (length args))
+          (probe-file (first args)))
+     (call-next-method))
+    (t 
+     (%stdout "invalid command argument: ~A~%" args))))
+
+
+(defmethod %perform-command ((command (eql :HELP)) args)
+  (help))
+
+(defun show-batch-completion-result (flag ordering result)
+  (when flag
+    (let* ((rules 
+             (rewrite-rule-set.rewrite-rules
+               (rename-for-human-readable-printing result)))
+           (variables                 
+             (remove-duplicates
+               (mapcan
+                 (lambda (x)
+                   (append
+                     (collect-variables
+                       (rewrite-rule.src x))
+                     (collect-variables
+                       (rewrite-rule.dst x))))
+                 rules)
+               :test #'term=)))
+      (%stdout "YES~%~%")
+      (%stdout "(VAR ~{~A~^ ~})~%" variables)
+      (%stdout "(RULES~%~{ ~A~%~})~%" rules)
+      (%stdout "(COMMENT~% ~A~%)~%" ordering)))
+  (unless flag
+    (%stdout "MAYBE~%")))
+
+
+(defmethod %perform-command ((command (eql :REWRITE)) args)
+  (let* ((fname (first args))
+         (terms (cdr args))
+         (result (%perform-command :COMPLETE (list fname))))
+    (when result
+      (let ((irreducible
+              (mapcar
+                (lambda (term)
+                  (let* ((parsed
+                           (parse-premise-logical-expression
+                             (format nil "~A = DUMMY" term)))
+                         ;; 項単体でのパースができない為、
+                         ;; ダミーの等式をパースしてから取り出す
+                         (parsed-term
+                           (equation.left 
+                             (first (clause.literals parsed)))))
+                    (list term (rewrite-final parsed-term result))))
+                terms)))
+        (%stdout "(COMMENT~%~:{ ~A reduced ~A~%~})~%" irreducible)))))
+
 
 (defmethod %perform-command ((command (eql :COMPLETE)) args)
   (let* ((fname (first args))
@@ -45,27 +109,8 @@
          (eqs (parse-mkbtt-expression content)))
     (multiple-value-bind (flag ordering result)
         (multi-kb-completion eqs 15)
-      (when flag
-        (let* ((rules 
-                 (rewrite-rule-set.rewrite-rules
-                   (rename-for-human-readable-printing result)))
-               (variables                 
-                 (remove-duplicates
-                   (mapcan
-                     (lambda (x)
-                       (append
-                         (collect-variables
-                           (rewrite-rule.src x))
-                         (collect-variables
-                           (rewrite-rule.dst x))))
-                     rules)
-                   :test #'term=)))
-          (%stdout "YES~%~%")
-          (%stdout "(VAR ~{~A~^ ~})~%" variables)
-          (%stdout "(RULES~%~{ ~A~%~})~%" rules)
-          (%stdout "(COMMENT~% ~A~%)~%" ordering)))
-      (unless flag
-        (%stdout "MAYBE")))))
+      (show-batch-completion-result flag ordering result)
+      result)))
 
 
 (defun main (args)
